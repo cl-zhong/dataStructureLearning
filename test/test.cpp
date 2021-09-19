3fa894abf5a8aa15b139a9389eb35e3946626b35
zhong <zhongcl@zju.edu.cn>
2021-09-19 09:38:36 +0800

    git test1

--- test/test.cpp
+++ test/test.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 int main()
 {
+    //git test
     LNode *A = (LNode *)malloc(sizeof(LNode));
     LNode *B = (LNode *)malloc(sizeof(LNode));
     LNode *C = (LNode *)malloc(sizeof(LNode));
